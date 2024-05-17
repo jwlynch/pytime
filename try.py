cffcc1f525882bea17724cad2c193d17349bedae
Jim <jim@jam.sessionsnet.org>
2024-05-17 00:26:22 -0700

    first try at getting a time in someone elses timezone to my own

---
+++ try.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#import pendulum
+#
+#n = pendulum.now()
+#
+#print(n)
+
+import datetime
+from datetime import datetime
+strptime = datetime.strptime
+
+e = strptime("6 PM", "")
+
+print(e)
