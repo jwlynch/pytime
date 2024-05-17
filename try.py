b5cb9efb8e02174668987ed788563c1b8a77453c
Jim <jim@jam.sessionsnet.org>
2024-05-17 00:50:51 -0700

    function interface

--- try.py
+++ try.py
@@ -6,10 +6,22 @@
 #
 #print(n)
 
-import datetime
-from datetime import datetime
-strptime = datetime.strptime
+#import datetime
+#from datetime import datetime
+#strptime = datetime.strptime
+#
+#e = strptime("6 PM", "")
+#
+#print(e)
 
-e = strptime("6 PM", "")
+# function parameter/result interface:
+#
+# time: string of the remote time
+#
+# timezone: timezone of remote time to parse time in
 
-print(e)
+def show_remote_time(time, timezone):
+    result_str = remote_time_in_local_timezone(time, timezone)
+
+    return result_str
+
